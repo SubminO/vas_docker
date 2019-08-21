5c733c19bf188b1f709d3aa40db412dbb402cd4a
Юрий Шикин <shikin@submin.ru>
2019-08-22 00:57:44 +0400

    косякнул с переменной

--- certbot/renew_cert.sh
+++ certbot/renew_cert.sh
@@ -7,7 +7,7 @@ echo "certbot_regru:dns_password='$REGRUDNS_PASS'" >> $CREDENTIALS
 
 chmod 600 $CREDENTIALS
 
-certbot certonly -n --agree-tos --email $LENC_EMAIL -a certbot-regru:dns -d $REGRUDNS_DOMAIN \
+certbot certonly -n --agree-tos --email $REGRUDNS_EMAIL -a certbot-regru:dns -d $REGRUDNS_DOMAIN \
     --certbot-regru:dns-credentials $CREDENTIALS
 
 cp  --recursive /etc/letsencrypt/* /root/certs/
