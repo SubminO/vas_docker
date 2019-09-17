2dc8005ddf791a45dc88e9b933ae4d3365e438b6
Юрий Шикин <shikin@submin.ru>
2019-09-17 13:46:22 +0400

    AMQP service require credentials and change wsaddr to translator

--- runfiles/run_keeper.sh
+++ runfiles/run_keeper.sh
@@ -7,6 +7,7 @@
 
 cd /opt/keeper
 
-python ./run.py --wsaddr $WS_ADDR --wsport $WS_PORT --gbhost $GDB_HOST --gbport $GDB_PORT \
+python ./run.py --wsaddr "translator" --wsport $WS_PORT --gbhost $GDB_HOST --gbport $GDB_PORT \
  --gbdb $GDB_DBNAME --location $LOCATION --rradius $ROUTE_RADIUS --pradius $PLATFORM_RADIUS \
- --dbshost $DBS_HOST --dbsuser $DBS_USER --dbspass $DBS_PASS --dbsdb $DBS_DBNAME --rmqhost $RMQ_HOST
+ --dbshost $DBS_HOST --dbsuser $DBS_USER --dbspass $DBS_PASS --dbsdb $DBS_DBNAME --rmqhost $RMQ_HOST \
+ --rmquser $RMQ_USER --rmqpass  $RMQ_PASS
